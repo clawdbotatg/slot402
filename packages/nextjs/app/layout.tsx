f28bb97f87c2fb41a7df549b1f64667d688ea160
Austin Griffith <austin@ethereum.org>
2025-10-29 11:40:23 -0600

    branding

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -5,8 +5,8 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Scaffold-ETH 2 App",
-  description: "Built with 🏗 Scaffold-ETH 2",
+  title: "Based Slot",
+  description: "A decentralized slot machine built on Base",
 });
 
 export const viewport = {
